995eb08a6c36c0ce83f89524643112089f712ced
palak-parhawk-2003 <pparhawk@gmail.com>
2025-06-20 01:45:24 +0530

    render backend link added to frontend

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000', {
+const socket = io('https://live-polling-system-backend-ssya.onrender.com', {
   transports: ['websocket'],
 });
 
